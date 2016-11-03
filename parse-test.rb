09125001a05022c090cea616ae9c84fd794b5118
Jens Skott <jens.skott@gmail.com>
2016-11-03 13:32:10 +0100

    made some updated to erb templates

--- parse-test.rb
+++ parse-test.rb
@@ -4,18 +4,8 @@ require 'erb'
 opts = YAML.load_file(ARGV.shift)
 env = ARGV.shift
 serviceName = opts['service']['name'] + "-#{env}"
-if !opts['task'][env]['require'].nil?
-  link = opts['task'][env]['childs'][opts['task'][env]['require']]['name']
-  puts link
-end
 
-puts env
-puts serviceName
-
-puts "\nNow comes all the values\n"
-puts opts.inspect
-
-task_definition = "container_definitions: [
+task_definition = "{container_definitions: [
     {
       <% opts['task'][env].each do |k,v| %>
       <%= k %>: <%= v %>,
@@ -23,6 +13,8 @@ task_definition = "container_definitions: [
       privileged: false
     },
   ],
+  family: \"<%= serviceName %>\",
+}
 "
 
 renderer = ERB.new(task_definition)
