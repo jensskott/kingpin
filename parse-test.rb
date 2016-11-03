a4285c6277bc1306c1afdf605a1adf09b549362a
Jens Skott <jens.skott@gmail.com>
2016-11-03 15:56:50 +0100

    started to fix task definition

--- parse-test.rb
+++ parse-test.rb
@@ -1,21 +1,26 @@
 require 'yaml'
 require 'erb'
+require 'json'
 
 opts = YAML.load_file(ARGV.shift)
 env = ARGV.shift
 serviceName = opts['service']['name'] + "-#{env}"
 
-task_definition = "{container_definitions: [
+task_definition = "\"{container_definitions\": [
+    <% opts['task'][env].each do |k,v| %>
     {
-      <% opts['task'][env].each do |k,v| %>
-      <%= k %>: <%= v %>,
+      <% v.each do |k,v| %>
+      \"<%= k %>\": \"<%= v %>\",
       <% end %>
-      privileged: false
+      \"privileged\": false
     },
+    <% end %>
   ],
-  family: \"<%= serviceName %>\",
+  \"family\": \"<%= serviceName %>\",
 }
 "
 
 renderer = ERB.new(task_definition)
-puts renderer.result()
+File.open('task-definition.json', 'w') do |f|
+  f.write renderer.result(binding)
+end
